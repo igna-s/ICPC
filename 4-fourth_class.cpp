205a35dc979858d5cc1cdc9f1d19b570dadff755
Ignacio A. Schwindt <ignacio.schwindt.dev@gmail.com>
2025-05-16 16:36:02 -0300

    Update 4-fourth_class.cpp

--- 4-fourth_class.cpp
+++ 4-fourth_class.cpp
@@ -255,3 +255,48 @@ int main() {
     return 0;
 }
 
+
+
+// F - Counting Rooms - CSES
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void bfs(int i, int j, int n, int m, vector<vector<bool>> &vis, vector<vector<char>> &a) {
+    if (i < 0 || i >= n || j < 0 || j >= m || vis[i][j] || a[i][j] == '#')
+        return;
+
+    vis[i][j] = true;
+
+    bfs(i - 1, j, n, m, vis, a);
+    bfs(i + 1, j, n, m, vis, a);
+    bfs(i, j - 1, n, m, vis, a);
+    bfs(i, j + 1, n, m, vis, a);
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>> a(n, vector<char>(m));
+    vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> a[i][j];
+
+    int maxi = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == '.' && !vis[i][j]) {
+                bfs(i, j, n, m, vis, a);
+                maxi++;
+            }
+        }
+    }
+
+    cout << maxi << "\n";
+    return 0;
+}
+
