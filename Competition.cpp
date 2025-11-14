f517446f3f958f8b22eb846d11b6036c28bb50ed
Ignacio A. Schwindt <ignacio.schwindt.dev@gmail.com>
2025-11-14 20:55:35 -0300

    Update Competition.cpp
    Added links to photos of the team and event.

--- Competition.cpp
+++ Competition.cpp
@@ -15,3 +15,11 @@
 
 
 //Final result: 147
+
+
+
+//Buenas, por pedido general, compartimos links con fotos de la regional
+
+//Fotos de equipo https://drive.google.com/drive/folders/1cbYGKZjRzZMLSbMGuhhvRzXeC6EeSWAU
+
+//Fotos del evento https://drive.google.com/drive/folders/1b-ZF74h95M484esGiywSXz6PeU6z0xzm
